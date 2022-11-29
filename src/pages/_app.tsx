e3428a9efcd0226a73aac5d56aa61ea31c8ca6c1
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-11-29 12:56:18 +0100

    fix introduced lint errors

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,10 @@
+import "../styles/globals.css";
+
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 
 import { trpc } from "../utils/trpc";
-
-import "../styles/globals.css";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
